c24196fc7a1f86125fb3798479c062f28ceb1480
KangQull <110822952+KangQull@users.noreply.github.com>
2024-10-26 18:37:41 +0700

    Update restoress3.sh

--- restoress3.sh
+++ restoress3.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 echo 'Started Restore to your Spaces VPS'
 date +'%a %b %e %H:%M:$S %Z %Y'
-s3cmd get -r s3://bkvps/srv /srv
+s3cmd get -r s3://bkvps/srv/ /srv
+s3cmd get -r s3://bkvps/etc/ /etc
+s3cmd get -r s3://bkvps/home/ /home
+s3cmd get -r s3://bkvps/var/ /var
 date +'%a %b %e %H:%M:$S %Z %Y'
 echo 'Finished Restore to your Spaces VPS'
