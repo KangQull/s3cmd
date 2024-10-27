7fc77fff67d58356c66914b9e56243ae4c2652e2
KangQull <110822952+KangQull@users.noreply.github.com>
2024-10-27 09:40:44 +0700

    Update s3cmd.sh

--- s3cmd.sh
+++ s3cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Update package repositories
-apt-get update && apt-get upgrade -y
+apt-get update
 
 # Install S3CMD Tools
 apt install s3cmd -y
@@ -9,17 +9,19 @@ apt install s3cmd -y
 # Configurasi s3cmd
 s3cmd --configure
 
+read -p "Masukan Nama S3 Bucket kamu: " bucket
+
 # Proses buat Script backup
-echo "echo 'Started Sync to Digitalocean Spaces'
+echo "echo 'Started Sync to Your Spaces'
 date +'%a %b %e %H:%M:$S %Z %Y'
-s3cmd sync --recursive --preserve /srv  s3://bucket-kamu
-s3cmd sync --recursive --preserve /etc  s3://bucket-kamu
-s3cmd sync --recursive --preserve /home s3://bucket-kamu
-s3cmd sync --recursive --preserve /var  s3://bucket-kamu
+s3cmd sync --recursive --preserve /srv  s3://$bucket
+s3cmd sync --recursive --preserve /etc  s3://$bucket
+s3cmd sync --recursive --preserve /home s3://$bucket
+s3cmd sync --recursive --preserve /var  s3://$bucket
 dpkg --get-selections > dpkg.list
-s3cmd sync --recursive --preserve dpkg.list s3://bucket-kamu
+s3cmd sync --recursive --preserve dpkg.list s3://$bucket
 date +'%a %b %e %H:%M:$S %Z %Y'
-echo 'Finished Sync to Digitalocean Spaces' " > ~/s3cmdbcp.sh
+echo 'Finished Sync to Your Spaces' " > ~/s3cmdbcp.sh
 
 # Menjalan kan Script backup
 bash s3cmdbcp.sh
