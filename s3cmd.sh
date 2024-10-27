815edefe5c9e306dc1b6cf34f6b5da199a36f479
KangQull <110822952+KangQull@users.noreply.github.com>
2024-10-27 08:25:13 +0700

    Update s3cmd.sh

--- s3cmd.sh
+++ s3cmd.sh
@@ -9,3 +9,9 @@ apt install s3cmd -y
 # Configurasi s3cmd
 s3cmd --configure
 
+# Download Script backup
+wget https://raw.githubusercontent.com/KangQull/s3cmd/main/s3cmdbk.sh
+
+# Menjalan kan Script backup
+sh s3cmdbk.sh
+
