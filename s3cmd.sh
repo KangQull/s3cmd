8957b76513c04c8eb5323040f8dea95459d3d740
KangQull <110822952+KangQull@users.noreply.github.com>
2024-10-27 09:19:56 +0700

    Update s3cmd.sh

--- s3cmd.sh
+++ s3cmd.sh
@@ -9,9 +9,18 @@ apt install s3cmd -y
 # Configurasi s3cmd
 s3cmd --configure
 
-# Download Script backup
-wget https://raw.githubusercontent.com/KangQull/s3cmd/main/s3cmdbk.sh
+# Proses buat Script backup
+echo "echo 'Started Sync to Digitalocean Spaces'
+date +'%a %b %e %H:%M:$S %Z %Y'
+s3cmd sync --recursive --preserve /srv  s3://bucket-kamu
+s3cmd sync --recursive --preserve /etc  s3://bucket-kamu
+s3cmd sync --recursive --preserve /home s3://bucket-kamu
+s3cmd sync --recursive --preserve /var  s3://bucket-kamu
+dpkg --get-selections > dpkg.list
+s3cmd sync --recursive --preserve dpkg.list s3://bucket-kamu
+date +'%a %b %e %H:%M:$S %Z %Y'
+echo 'Finished Sync to Digitalocean Spaces' " > ~/s3cmdbcp.sh
 
 # Menjalan kan Script backup
-sh s3cmdbk.sh
+bash s3cmdbcp.sh
 
